3fd8bf6ae90890d2b07cad3b15b53af6d0c49d18
Rémi GATTAZ <remi.gattaz@free.fr>
2014-03-01 17:13:00 -0800

    Add the database dump file

--- README.md
+++ README.md
@@ -27,11 +27,11 @@ Installation
 
 2) Install the ruby gem call berkshelf ``` gem install berkshelf ``` (might require sudo depending on your ruby installation)
 
-3) Install the vagrant plugins berkshelf, hostmanager and omnibus 
+3) Install the vagrant plugins berkshelf, hostmanager and omnibus
 ```
 vagrant plugin install vagrant-berkshelf
 vagrant plugin install vagrant-hostmanager
-vagrant plugin install vagrant-omnibus 
+vagrant plugin install vagrant-omnibus
 ```
 
 4) Run the command ```vagrant up --provision``` in the just created directory. It will create the vm.
@@ -42,13 +42,16 @@ How to use
 ----------
 Here are the basic commands of vagrant :
 
-- ```vagrant up --provision``` : start or restart a vagrant vm
+- ```vagrant up``` : start or restart a vagrant vm
+- ```vagrant provision``` : provisions the vm. It will execute the commands in the vagrant file.
 - ```vagrant suspend``` : suspend the vagrant vm in virtualbox
 - ```vagrant halt``` : halt the vagrnat vm in virtualbox
 - ```vagrant destroy``` : delete a vagrant vm from virtualbox
 - ```vagrant ssh``` : open a ssh shell to the vagrant vm
 
 It is better to suspend or halt the vm after every use because restarting your computer with a live vagrant vm can create errors.
+
+Every time the vm is provisionned, the database will be destroyed and recreated.
 
 See the website
 -------------
--- config/dump.sql
+++ config/dump.sql
@@ -1,1 +1,130 @@
+/* This file is divided in several parts. They are here described
+ 1) Cleaning of the database 
+ 2) Creation of the table 
+ 3) Creation of the primary keys 
+ 4) Creation of the unique constaints 
+ 5) Creation of the foreign keys 
 
+Except for the first part, in every part, the creation of elements is made in alphabetic order 
+*/
+
+/* PART 1 : Cleaning */
+DROP TABLE IF EXISTS PermissionQuizzes;
+DROP TABLE IF EXISTS PermissionCourses;
+DROP TABLE IF EXISTS UserAnswers;
+DROP TABLE IF EXISTS Answers;
+DROP TABLE IF EXISTS UserQuestions;
+DROP TABLE IF EXISTS Questions;
+DROP TABLE IF EXISTS QuizSessions;
+DROP TABLE IF EXISTS Quizzes;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Users;
+
+
+/* PART 2 : Creation of the tables */
+CREATE TABLE IF NOT EXISTS Answers(
+id integer,
+question_id integer NOT NULL,
+number integer NOT NULL,
+value varchar(255) NOT NULL,
+correct boolean NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Courses(
+id integer,
+name varchar(255) NOT NULL,
+user_id integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PermissionCourses(
+course_id integer NOT NULL,
+user_id integer NOT NULL,
+permission integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PermissionQuizzes(
+quiz_id integer NOT NULL,
+user_id integer NOT NULL,
+permission integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Questions(
+id integer,
+number integer NOT NULL,
+quiz_id integer NOT NULL,
+type integer NOT NULL,
+question varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS QuizSessions(
+id integer,
+quiz_id integer NOT NULL,
+user_id integer NOT NULL,
+date_begin date NOT NULL,
+status integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Quizzes(
+id integer,
+name varchar(255) NOT NULL,
+course_id integer NOT NULL,
+parentquiz integer,
+user_id integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS UserAnswers(
+id integer NOT NULL,
+userquestion_id integer NOT NULL,
+answer_id integer NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS UserQuestions(
+id integer NOT NULL,
+quizsession_id integer NOT NULL,
+user_id integer NOT NULL,
+question_id integer
+);
+
+CREATE TABlE IF NOT EXISTS Users(
+id integer,
+email varchar(255) NOT NULL,
+firstname varchar(255),
+lastname varchar(255)
+);
+
+
+/* PART 3 : Creation of the PRIMARY KEYS */
+ALTER TABLE Answers ADD CONSTRAINT pk_Answers_id PRIMARY KEY (id);
+ALTER TABLE Courses ADD CONSTRAINT pk_Courses_id PRIMARY KEY (id);
+ALTER TABLE PermissionCourses ADD CONSTRAINT pk_PermissionCourses_id PRIMARY KEY (course_id, user_id, permission);
+ALTER TABLE PermissionQuizzes ADD CONSTRAINT pk_PermissionQuiz_id PRIMARY KEY (quiz_id, user_id, permission);
+ALTER TABLE Questions ADD CONSTRAINT pk_Questions_id PRIMARY KEY (id);
+ALTER TABLE QuizSessions ADD CONSTRAINT pk_QuizSessions_id PRIMARY KEY (id);
+ALTER TABLE Quizzes ADD CONSTRAINT pk_Quizzes_id PRIMARY KEY (id);
+ALTER TABLE UserAnswers ADD CONSTRAINT pk_UserAnswers_id PRIMARY KEY (id);
+ALTER TABLE UserQuestions ADD CONSTRAINT pk_UserQuestions_id PRIMARY KEY (id);
+ALTER TABLE Users ADD CONSTRAINT pk_Users_id PRIMARY KEY (id);
+
+
+/* PART 3 : Creation of the UNIQUE constaints */
+ALTER TABLE Answers ADD CONSTRAINT uk_Answers_number UNIQUE (id, number);
+ALTER TABLE Questions ADD CONSTRAINT uk_Questions_number UNIQUE (id, number);
+ALTER TABLE Users ADD CONSTRAINT uk_Users_email UNIQUE (email);
+
+/* PART 4 : Creation of the FOREIGN KEYS */
+ALTER TABLE Answers ADD CONSTRAINT fk_Answers_question FOREIGN KEY (question_id) REFERENCES Questions(id);
+ALTER TABLE Courses ADD CONSTRAINT fk_Courses_user FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE PermissionCourses ADD CONSTRAINT fk_PermissionCourses_user FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE PermissionCourses ADD CONSTRAINT fk_PermissionCourses_course FOREIGN KEY (course_id) REFERENCES Courses(id);
+ALTER TABLE PermissionQuizzes ADD CONSTRAINT fk_PermissionQuizzes_quiz FOREIGN KEY (quiz_id) REFERENCES Quizzes(id);
+ALTER TABLE PermissionQuizzes ADD CONSTRAINT fk_PermissionQuizzes_user FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE Questions ADD CONSTRAINT fk_Questions_quiz FOREIGN KEY (quiz_id) REFERENCES Quizzes(id);
+ALTER TABLE QuizSessions ADD CONSTRAINT fk_QuizSessions_quiz FOREIGN KEY (quiz_id) REFERENCES Quizzes(id);
+ALTER TABLE QuizSessions ADD CONSTRAINT fk_QuizSessions_user FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE Quizzes ADD CONSTRAINT fk_Quizzes_course FOREIGN KEY (course_id) REFERENCES Courses(id);
+ALTER TABLE Quizzes ADD CONSTRAINT fk_Quizzes_user FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE UserAnswers ADD CONSTRAINT fk_UserAnswers_answer FOREIGN KEY (answer_id) REFERENCES Answers(id);
+ALTER TABLE UserAnswers ADD CONSTRAINT fk_UserAnswers_userquestion FOREIGN KEY (userquestion_id) REFERENCES UserQuestions(id);
+ALTER TABLE UserQuestions ADD CONSTRAINT fk_UserQuestions_question FOREIGN KEY (question_id) REFERENCES Questions(id);
+ALTER TABLE UserQuestions ADD CONSTRAINT fk_UserQuestions_quizsession FOREIGN KEY (quizsession_id) REFERENCES QuizSessions(id);
+ALTER TABLE UserQuestions ADD CONSTRAINT fk_UserQuestions_user FOREIGN KEY (user_id) REFERENCES Users(id);
